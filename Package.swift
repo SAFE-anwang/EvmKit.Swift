cfa455a2cc058e7d4136d477e929bd5dd06518fb
SL Jiang <271398741@qq.com>
2025-10-10 19:26:56 +0800

    update version for web3swift

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         .package(url: "https://github.com/horizontalsystems/HdWalletKit.Swift.git", .upToNextMajor(from: "1.2.1")),
         .package(url: "https://github.com/horizontalsystems/HsToolKit.Swift.git", .upToNextMajor(from: "2.0.3")),
         .package(url: "https://github.com/horizontalsystems/HsExtensions.Swift.git", .upToNextMajor(from: "1.0.6")),
-        .package(url: "https://github.com/SAFE-anwang/web3swift.git", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/SAFE-anwang/web3swift.git", .upToNextMajor(from: "2.0.29")),
     ],
     targets: [
         .target(
